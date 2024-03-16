596ae83b7abee6ab562a1e801c1669cefb0b1b0e
oleksii.babii <oleksii.babii@student.griffith.ie>
2024-03-16 05:44:08 +0000

    Add a folder for usit tests

---
+++ src/test/java/griffith/ChefConnectTest.java
@@ -0,0 +1,14 @@
+package griffith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ChefConnectTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
