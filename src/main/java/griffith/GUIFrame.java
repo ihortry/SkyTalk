e064acab43245d81de681d4f3e8dc71e23e04562
Ihor Tryndey <igortryndey@gmail.com>
2024-03-17 20:08:14 +0000

    GUIFrame class creation

---
+++ src/main/java/griffith/GUIFrame.java
@@ -0,0 +1,18 @@
+/**
+ * @author Ihor Tryndey 3105023 , Oleksii Babii 3104904
+ * @since 2024
+ * @version 1.0
+ */
+package griffith;
+
+import javax.swing.JTextField;
+
+/**
+ * @author Ihor Tryndey 3105023 , Oleksii Babii 3104904
+ * Class RoundTextField which extends JTextField 
+ * The main idea of this class is to create round text field to enter a statement 
+ * inside chat bot
+ */
+public class GUIFrame extends JTextField{
+
+}
