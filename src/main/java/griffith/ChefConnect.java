659d2ee80c71ac12f70e46c639a9be239af3bf51
oleksii.babii <oleksii.babii@student.griffith.ie>
2024-03-17 13:31:49 +0000

    fix: rewrite provideNutritionalInfo() for using API

--- src/main/java/griffith/ChefConnect.java
+++ src/main/java/griffith/ChefConnect.java
@@ -59,34 +59,69 @@ public class ChefConnect {
 			System.out.println("Enter the name of the dish: ");
 			Scanner input = new Scanner(System.in);
 			String dish = input.nextLine();
-//          provideNutritionalInfo(dish);
+			System.out.println(provideNutritionalInfo(dish));
 			return ""; // Response handled within the method
 		}
 
 		return "I'm sorry, I didn't understand your request. Could you please provide more details?";
 	}
+	
+	public static String provideNutritionalInfo(String userInput) {
 
-    public static JsonNode getRootNode(String userInput) {
-    	try {
-    		// Make HTTP request to the recipe API
-            userInput = URLEncoder.encode(userInput, StandardCharsets.UTF_8);
-            URL url = new URL(RECIPE_API_URL + "?q=" + userInput + "&app_id=" + APP_ID + "&app_key=" + APP_KEY);
-            //System.out.println(url);
-            Scanner scanner = new Scanner(url.openStream());
-            StringBuilder responseBuilder = new StringBuilder();
-            while (scanner.hasNextLine()) {
-                responseBuilder.append(scanner.nextLine());
-            }
-            scanner.close();
+		JsonNode rootNode = getRootNode(userInput);
+		JsonNode hitsNode = rootNode.get("hits");
+		if (hitsNode != null && hitsNode.isArray() && hitsNode.size() > 0) {
+			// Get the first recipe from the response
+			JsonNode recipeNode = hitsNode.get(0).get("recipe");
+			JsonNode labelNode = recipeNode.get("label");
+			JsonNode urlNode = recipeNode.get("url");
 
-            // Parse JSON response
-            ObjectMapper mapper = new ObjectMapper();
-            return mapper.readTree(responseBuilder.toString());
-    	} catch (Exception e) {
-    		e.printStackTrace();
+			if (labelNode != null && urlNode != null) {
+				String recipeName = labelNode.asText();
+				double hundredGramParts = recipeNode.get("totalWeight").asDouble() / 100.0;
+				JsonNode caloriesNode = recipeNode.get("calories");
+				String calories = String.format("%.02f", caloriesNode.asDouble() / hundredGramParts);
+				JsonNode totalNutrientsNode = recipeNode.get("totalNutrients");
+				JsonNode fatQuantity = totalNutrientsNode.get("FAT").get("quantity");
+
+				// String fatQuantity = fatNode.get("quantity").asText();
+				String fat = String.format("%.02f", fatQuantity.asDouble() / hundredGramParts);
+				JsonNode proteinQuantity = totalNutrientsNode.get("PROCNT").get("quantity");
+				String protein = String.format("%.02f", proteinQuantity.asDouble() / hundredGramParts);
+
+				return "Nutritional Information:\n" + " Calories: " + calories + " kcal\n" + " Protein: " + protein
+						+ " g\n" + " Fat: " + fat + " g\n";
+			} else {
+				return "Sorry, I couldn't find the necessary nutritional information.";
+			}
+
+		} else {
+			return "Sorry, I couldn't find any nutritional information for your query.";
+		}
+
+	}
+
+	public static JsonNode getRootNode(String userInput) {
+		try {
+			// Make HTTP request to the recipe API
+			userInput = URLEncoder.encode(userInput, StandardCharsets.UTF_8);
+			URL url = new URL(RECIPE_API_URL + "?q=" + userInput + "&app_id=" + APP_ID + "&app_key=" + APP_KEY);
+			// output(url);
+			Scanner scanner = new Scanner(url.openStream());
+			StringBuilder responseBuilder = new StringBuilder();
+			while (scanner.hasNextLine()) {
+				responseBuilder.append(scanner.nextLine());
+			}
+			scanner.close();
+
+			// Parse JSON response
+			ObjectMapper mapper = new ObjectMapper();
+			return mapper.readTree(responseBuilder.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		return null;
-    	
-    }
+
+	}
 
 }
