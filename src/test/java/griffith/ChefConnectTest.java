55ae21f9fd9e5cc17163feeca790c70628a5212b
Ihor Tryndey <igortryndey@gmail.com>
2024-03-27 15:09:37 +0000

    parametrized test template implementation

--- src/test/java/griffith/ChefConnectTest.java
+++ src/test/java/griffith/ChefConnectTest.java
@@ -1,13 +1,40 @@
+/**
+ * @author Ihor Tryndey 3105023 , Oleksii Babii 3104904
+ * @since 2024
+ * @version 1.0
+ */
 package griffith;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.Collection;
 
-class ChefConnectTest {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
+public class ChefConnectTest {
+
+	
+	private String input;
+	private String expectedOutput;
+	
+	public ChefConnectTest(String input, String expectedOutput) {
+		super();
+		this.input = input;
+		this.expectedOutput = expectedOutput;
+	}
+	@Parameters
+	public static Collection<String[]> testConditions(){
+		String expectedOutput[][] = {{"",""},{"", ""},{"", ""},{"", ""}}; 
+							  //actual, expected
+		return Arrays.asList(expectedOutput);
+	}
 	@Test
-	void test() {
+	public void test() {
 		fail("Not yet implemented");
 	}
 
