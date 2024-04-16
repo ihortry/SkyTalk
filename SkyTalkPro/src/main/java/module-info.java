3fda4312de5bddcf64a4efccd1909884b8820b2e
oleksii.babii <oleksii.babii@student.griffith.ie>
2024-04-16 20:24:55 +0100

    feat: set up default javaFX Maven project

---
+++ SkyTalkPro/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module griffith.skytalkpro {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens griffith.skytalkpro to javafx.fxml;
+    exports griffith.skytalkpro;
+}
