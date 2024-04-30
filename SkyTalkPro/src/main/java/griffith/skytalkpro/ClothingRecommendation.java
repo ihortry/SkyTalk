f93632a423be934338ec0c77aa95ff3957bc63d6
dessie <lynxes.desmond@gmail.com>
2024-04-30 17:28:51 +0100

    add comments to clothing recommendation method

--- SkyTalkPro/src/main/java/griffith/skytalkpro/ClothingRecommendation.java
+++ SkyTalkPro/src/main/java/griffith/skytalkpro/ClothingRecommendation.java
@@ -5,14 +5,17 @@ public class ClothingRecommendation {
     public static String generateClothingPlan(double minTemperature, double maxTemperature, boolean umbrellaIsNeeded,
                                               boolean sunglassesIsNeeded) {
 
+        //return error string if minimum temperature is higher than maximum temperature
         if(minTemperature > maxTemperature) {
             return "Not defined";
         }
+        //build a string with the minimum and maximum temperatures
         StringBuilder plan = new StringBuilder();
         plan.append("My suggestion:\n" + "Since the lowest temperature during the entire trip will be " + minTemperature
                 + " degrees\nCelsius and the highest " + maxTemperature + " degrees Celsius.\n" + " Put on a ");
 
         // "Top" clothes
+        //decides which clothing string to append to the builder based on the temperature
         if (minTemperature > 15) {
             if (maxTemperature < 25) {
                 plan.append("T-Shirt ");
@@ -44,10 +47,12 @@ public class ClothingRecommendation {
             plan.append("and Jeans.\n");
         }
 
+        //if it's going to rain, recommend an umbrella
         if (umbrellaIsNeeded) {
             plan.append(" There is a high chance of rain during your trip,\n so take an umbrella or a raincoat.🌧");
         }
 
+        //if cloud cover is low, recommend sunglasses
         if (sunglassesIsNeeded) {
             plan.append("Don't forget to bring your sunglasses, you'll need them.☼");
         }
